bb269477b34d99adbb39eb10240b15f5d459a479
aishikanandi <aishikanandi@gmail.com>
2023-07-08 02:15:52 +0530

    Landing page commit

--- src/landingPage.js
+++ src/landingPage.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
 import './landingPage.css';
+import React from 'react';
+import Navbar from "./navbar";
+import Footer from "./footer";
 
-function App() {
-  return (
-    <div className="landingPage">
-
-    </div>
-  );
+function LandingPage() {
+	return (
+		<React.Fragment className="landingPage">
+      <div className="waveUp"/>
+			<Navbar className='navBar' />
+      <img className="centerLogo" height="500%" alt="" src="openlakelogo.svg"></img>
+      <Footer className="footer"/>
+		</React.Fragment>
+	);
 }
 
-export default App;
+export default LandingPage;
