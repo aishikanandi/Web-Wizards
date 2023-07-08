c49db6675021cdb55bd67c9157d86e5a2c5918c4
vinayak180203 <vinayakk@iitbhilai.ac.in>
2023-07-09 01:43:46 +0530

    Completed Programs Timeline page with one-two things to be changed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LandingPage from './landingPage';
+// import LandingPage from './landingPage';
+import ProgramTimeline from './programTimeline';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LandingPage />
+    <ProgramTimeline />
   </React.StrictMode>
 );
 
--- src/programTimeline.js
+++ src/programTimeline.js
@@ -1,8 +1,42 @@
 import React,{useEffect} from 'react';
 import './programTimeline.css';
-// import Navbar from "./navbar";
-// import Footer from "./footer";
+import Navbar from "./navbar";
+import Footer from "./footer";
+import './landingPage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareArrowUpRight } from '@fortawesome/free-solid-svg-icons';
 
+
+const programs = [
+    {
+        "id": 1,
+        "name": "",
+        "date": "15th September, 2023",
+        "host": "Tushar Bansal",
+        "description": ""
+    },
+    {
+        "id": 2,
+        "name": "Webwave",
+        "date": "1st July, 2023 ",
+        "host": "Aditya Dubey and Arnav Prakash",
+        "description": "Create a website for Openlake club."
+    },
+    {
+        "id": 3,
+        "name": "FOSS Overflow",
+        "date": "27th November, 2023",
+        "host": "Satvik Vemuganti",
+        "description": "Building open source softwares for use within the institute."
+    },
+    {
+        "id": 4,
+        "name": "Zapricorn",
+        "date": "6th November, 2023",
+        "host": "Satvik Vemuganti",
+        "description": "Building an Insti-app for IIT Bhilai."
+    }
+]
 function ProgramTimeline(){
     useEffect(() => {
         let prevScrollY = window.scrollY;
@@ -52,79 +86,38 @@ function ProgramTimeline(){
         };
       }, []);
     return(
-        <div className="programTimeline">
+        <div className = "programTimeline">
+            <div className="waveUp" />
+            <Navbar className="navBar" />
             <div className='container'>
             <div className='top-section'>
                 <h1 className='heading'>PROGRAMS</h1>
             </div>
             <div className='timeline'>
                 <div className='line'></div>
-                <div className='section'>
-                <div className='bead'><svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
-<circle cx="17" cy="17" r="17" transform="rotate(90 17 17)" fill="#FFFDFD"/>
-</svg></div>
-                <div className='content'>
-                    <div className='big-box'>
-                        <h2>FOSS Overflow</h2>
-                        <div className='small-box'>
-                            <p>Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, </p>
-                            <button className='detailsButton'>KNOW MORE 
-                            </button>
-                        </div>
-                    </div>
-                    {/* <div className='date'>6th April, 2023</div> */}
+                {programs.map((program) => (
+            <div className='section' key={program.id}>
+              <div className='bead'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
+                  <circle cx="17" cy="17" r="17" transform="rotate(90 17 17)" fill="#FFFDFD" />
+                </svg>
+              </div>
+              <div className='date'>{program.date}</div>
+              <div className='content'>
+                <div className='big-box'>
+                  <h2>{program.name}</h2>
+                  <div className='small-box'>
+                    <p>Host - {program.host} <br /> {program.description}</p>
+                    <button className='detailsButton'>KNOW MORE <FontAwesomeIcon icon={faSquareArrowUpRight} />
+                    </button>
+                  </div>
                 </div>
-                </div>
-                <div className='section'>
-                <div className='bead'><svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
-<circle cx="17" cy="17" r="17" transform="rotate(90 17 17)" fill="#FFFDFD"/>
-</svg></div>
-                <div className='content'>
-                    <div className='big-box'>
-                        <h2>FOSS Overflow</h2>
-                        <div className='small-box'>
-                            <p>Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, </p>
-                            <button className='detailsButton'>KNOW MORE 
-                            </button>
-                        </div>
-                    </div>
-                    {/* <div className='date'>6th April, 2023</div> */}
-                </div>
-                </div>
-                <div className='section'>
-                <div className='bead'><svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
-<circle cx="17" cy="17" r="17" transform="rotate(90 17 17)" fill="#FFFDFD"/>
-</svg></div>
-                <div className='content'>
-                    <div className='big-box'>
-                        <h2>FOSS Overflow</h2>
-                        <div className='small-box'>
-                            <p>Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, </p>
-                            <button className='detailsButton'>KNOW MORE 
-                            </button>
-                        </div>
-                    </div>
-                    {/* <div className='date'>6th April, 2023</div> */}
-                </div>
-                </div>
-                <div className='section'>
-                <div className='bead'><svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
-<circle cx="17" cy="17" r="17" transform="rotate(90 17 17)" fill="#FFFDFD"/>
-</svg></div>
-                <div className='content'>
-                    <div className='big-box'>
-                        <h2>FOSS Overflow</h2>
-                        <div className='small-box'>
-                            <p>Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, </p>
-                            <button className='detailsButton'>KNOW MORE 
-                            </button>
-                        </div>
-                    </div>
-                    {/* <div className='date'>6th April, 2023</div> */}
-                </div>
-                </div>
+              </div>
+            </div>
+          ))}
             </div>
             </div>
+            <Footer className="footer" />
         </div>
     );
 }
