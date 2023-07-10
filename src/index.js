e30f3a0c0dc1e06ef262cd43f1e7f7da51ce3728
aishikanandi <aishikanandi@gmail.com>
2023-07-10 19:38:34 +0530

    Rocket fixed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import {  HashRouter, Routes, Route } from 'react-router-dom';
+// import LandingPage from './landingPage.js';
+// import ProgramTimeline from './programTimeline.js';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     {/* <HashRouter>
+//         <Routes>
+//         <Route exact path="/" component={LandingPage} />
+//         <Route exact path="/timeline" component={ProgramTimeline} />
+//         </Routes>
+//       </HashRouter> */}
+//       <LandingPage />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import {  HashRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './landingPage';
-import ProgramTimeline from './programTimeline';
+import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <HashRouter>
-        {/* <Routes> */}
-        <Route exact path="/" component={LandingPage} />
-        {/* <Route exact path="/timeline" component={ProgramTimeline} />
-        </Routes> */}
-      </HashRouter>
+    
+      <App />
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
--- src/landingPage.js
+++ src/landingPage.js
@@ -1,4 +1,5 @@
 import "./landingPage.css";
+import './components/waterText.css'
 import React from "react";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
