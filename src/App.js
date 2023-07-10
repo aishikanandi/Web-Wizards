b153aad96a3f82f91c8cbcb179b1c7c4826dda90
aishikanandi <aishikanandi@gmail.com>
2023-07-10 22:56:09 +0530

    Routing for 3 pages done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import LandingPage from "./landingPage";
 import ProgramTimeline from "./programTimeline";
 import Program from "./Program";
-
+import Projects from "./Projects";
 function App() {
     return (
       <BrowserRouter>
@@ -10,6 +10,7 @@ function App() {
         <Route path='/' element={<LandingPage />} />
         <Route path='/programtimeline' element={<ProgramTimeline />} />
         <Route path='/programtimeline/program' element={<Program />} />
+        <Route path='/projects' element={<Projects />} />
       </Routes>
       </BrowserRouter>
     );
